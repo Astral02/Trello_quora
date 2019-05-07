a97794d04ab81ea4b56252ea4713e211d7f0da40
emansi <mansielhance@gmail.com>
2019-05-07 23:28:50 +0530

    Third Commit : Completed code for "SignupBusinessService" for signup endpoint.

--- quora-service/src/main/java/com/upgrad/quora/service/business/SignupBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/SignupBusinessService.java
@@ -1,7 +1,36 @@
 package com.upgrad.quora.service.business;/* Create by Mansi Elhance */
 
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SignupBusinessService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private PasswordCryptographyProvider passwordCryptographyProvider=new PasswordCryptographyProvider();
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserEntity signup(UserEntity userEntity) throws SignUpRestrictedException {
+
+        String[] encryptedText = passwordCryptographyProvider.encrypt(userEntity.getPassword());
+        userEntity.setSalt(encryptedText[0]);
+        userEntity.setPassword(encryptedText[1]);
+        if(userDao.getUserByUsername(userEntity.getUsername())!=null)
+        {
+            throw new SignUpRestrictedException("SGR-001","Try any other Username, this Username has already been taken");
+        }
+        else if(userDao.getUserByEmail(userEntity.getEmail())!=null){
+            throw new SignUpRestrictedException("SGR-002","This user has already been registered, try with any other emailId");
+        }
+        else
+            return userDao.createUser(userEntity);
+    }
 }
