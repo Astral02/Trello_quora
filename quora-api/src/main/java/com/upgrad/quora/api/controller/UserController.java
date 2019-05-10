966a093dbd713051f08a358b5f717372b2f9c605
emansi <mansielhance@gmail.com>
2019-05-10 20:44:30 +0530

    Fifth commit checklist : 1) Completed AuthenticationBusinessService for "signin" endpoint. 2) Completed code for "signin" endpoint in UserController class.
    ---- signin endpoint class completed -----

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,15 +1,17 @@
 package com.upgrad.quora.api.controller;/* Create by Mansi Elhance */
 
 import com.upgrad.quora.api.model.SigninResponse;
-import com.upgrad.quora.api.model.SignoutResponse;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.AuthenticationBusinessService;
+import com.upgrad.quora.service.business.SignOutBusinessService;
 import com.upgrad.quora.service.business.SignupBusinessService;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
-import com.upgrad.quora.service.exception.SignOutRestrictedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Base64;
 import java.util.UUID;
 
 @RestController
@@ -26,6 +29,12 @@ public class UserController {
 
     @Autowired
     private SignupBusinessService signupBusinessService;
+
+    @Autowired
+    private AuthenticationBusinessService authenticationBusinessService;
+
+    @Autowired
+    private SignOutBusinessService signOutBusinessService;
 
     //signup endpoint
     @RequestMapping(method= RequestMethod.POST, path="/user/signup", consumes= MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -53,14 +62,33 @@ public class UserController {
     //signin endpoint
     @RequestMapping(method = RequestMethod.POST, path = "/user/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SigninResponse> signin(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
-        return new ResponseEntity(HttpStatus.OK);
+        byte[] decode = Base64.getDecoder().decode(authorization.split("Basic ")[1]);
+        String decodedText = new String(decode);
+        String[] decodedArray = decodedText.split(":");
+
+        //AuthenticationService authenticationService=new AuthenticationService();
+        UserAuthEntity userAuthEntity = authenticationBusinessService.authenticate(decodedArray[0], decodedArray[1]);
+
+        SigninResponse signinResponse=new SigninResponse();
+        signinResponse.setMessage("SIGNED IN SUCCESSFULLY");
+        signinResponse.setId(userAuthEntity.getUser_id().getUuid());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access-token", userAuthEntity.getAccess_token());
+
+        return new ResponseEntity<SigninResponse>(signinResponse,headers,HttpStatus.OK);
     }
 
     //signout endpoint
-    @RequestMapping(method = RequestMethod.POST, path = "/user/signout", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<SignoutResponse> signout(@RequestHeader("Access_Token") final String accessToken) throws SignOutRestrictedException {
-        return new ResponseEntity(HttpStatus.OK);
-    }
+//    @RequestMapping(method = RequestMethod.POST, path = "/user/signout", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//    public ResponseEntity<SignoutResponse> signout(@RequestHeader("Access_Token") final String accessToken) throws SignOutRestrictedException {
+//
+//        String[] bearerToken=accessToken.split("Bearer");
+//        UserEntity signedoutUserEntity=signOutBusinessService.signout(bearerToken[0]);
+//        SignoutResponse signoutResponse= new SignoutResponse();
+//        signoutResponse.setMessage("SIGNED OUT SUCCESSFULLY");
+//        signoutResponse.setId(signedoutUserEntity.getUuid());
+//        return new ResponseEntity<SignoutResponse>(signoutResponse,HttpStatus.OK);
+//    }
 
 }
 
